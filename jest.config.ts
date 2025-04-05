4b1858d60978edcf3456dddf16c6307acbc3dc1e
sugar2456 <sugar2456_0526@yahoo.co.jp>
2025-04-05 17:29:17 +0900

    テスト環境追加で設定

--- jest.config.ts
+++ jest.config.ts
@@ -7,6 +7,7 @@ import type {Config} from 'jest';
 
 const config: Config = {
   preset: 'ts-jest',
+  testEnvironment: 'jsdom',
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
@@ -91,7 +92,9 @@ const config: Config = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
