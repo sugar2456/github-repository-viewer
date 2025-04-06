8d2a1878d7b55cb81625b4f279a78656435c9172
sugar2456 <sugar2456_0526@yahoo.co.jp>
2025-04-07 00:57:12 +0900

    octokitがテストコードで実行できない不具合を修正

--- jest.config.ts
+++ jest.config.ts
@@ -187,7 +187,12 @@ const config: Config = {
 
   // A map from regular expressions to paths to transformers
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true, // ts-jestでESMを有効化
+      },
+    ],
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
