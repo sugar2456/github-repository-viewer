9bafec605698f0d00ae937c58014b986bb1bf670
sugar2456 <sugar2456_0526@yahoo.co.jp>
2025-04-06 22:14:19 +0900

    リポジトリ詳細を取得する処理を実装

--- lib/repositories/interfaces/githubApiRepositoryInterface.ts
+++ lib/repositories/interfaces/githubApiRepositoryInterface.ts
@@ -1,8 +1,15 @@
 import SearchResult from "../responses/searchResult";
+import RepositoryDetailResult from "../responses/repositoryDetailResult";
+
 export default interface GithubApiRepositoryInterface {
   searchRepositories(
     query: string,
     page: number,
     perPage: number,
   ): Promise<SearchResult[]>;
+
+  getRepositoryDetails(
+    owner: string,
+    repo: string,
+  ): Promise<RepositoryDetailResult>;
 }
