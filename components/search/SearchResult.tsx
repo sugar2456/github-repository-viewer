d94fd63363644716f65c33b5367006dd72c3bd1a
sugar2456 <sugar2456_0526@yahoo.co.jp>
2025-04-08 18:59:23 +0900

    メッセージが中央寄せになるように修正

--- components/search/SearchResult.tsx
+++ components/search/SearchResult.tsx
@@ -1,19 +1,32 @@
-'use client';
+"use client";
 
-import SearchResult from '@/lib/repositories/responses/searchResult';
+import SearchResult from "@/lib/repositories/responses/searchResult";
 
-export default function SearchResults({ results }: { results: SearchResult[] | null }) {
+export default function SearchResults({
+  results,
+}: {
+  results: SearchResult[] | null;
+}) {
   if (!results) {
-    return <p className="text-gray-600">検索結果がありません。</p>;
+    return (
+      <div className="flex items-center justify-center">
+        <p className="text-gray-600">検索結果がありません。</p>
+      </div>
+    );
   }
 
   return (
     <div className="flex items-center justify-center">
       <ul className="mt-6 w-full max-w-lg">
         {results.map((result) => (
-          <li key={result.id} className="p-4 bg-white shadow-md rounded-lg mb-4">
+          <li
+            key={result.id}
+            className="p-4 bg-white shadow-md rounded-lg mb-4"
+          >
             <h2 className="text-xl font-bold">{result.repositoryName}</h2>
-            <p className="text-gray-600">{result.description || 'No description available'}</p>
+            <p className="text-gray-600">
+              {result.description || "No description available"}
+            </p>
           </li>
         ))}
       </ul>
