e329932e6204a01d7eb2983739464a2b183f06c9
sugar2456 <sugar2456_0526@yahoo.co.jp>
2025-04-09 13:05:56 +0900

    無限スクロールロジックの実装

--- components/search/SearchComponent.tsx
+++ components/search/SearchComponent.tsx
@@ -3,17 +3,21 @@
 import { useState } from "react";
 import SearchRepositoryAction from "@/actions/search/search_repository_action";
 import SearchForm from "./SearchForm";
-import SearchResults from "./SearchResult";
+import SearchResultsList from "./SearchResult";
 import Loading from "@/app/loading";
-import SearchResult from "@/lib/repositories/responses/searchResult";
+import Repository from "@/lib/repositories/responses/repository";
 
 export default function SearchComponent() {
-  const [results, setResults] = useState<SearchResult[]>([]);
+  const [results, setResults] = useState<Repository[]>([]);
   const [loading, setLoading] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
   const [page, setPage] = useState(1);
+  const [lastPage, setLastPage] = useState(1);
 
   const handleSearch = async (formData: FormData) => {
     setLoading(true);
+    // 検索すると改めてページを1に戻す
+    setPage(1);
 
     const query = formData.get("q") as string;
     if (!query) {
@@ -21,16 +25,36 @@ export default function SearchComponent() {
       return;
     }
   
-    const data = await SearchRepositoryAction(formData);
+    const result = await SearchRepositoryAction(formData);
   
-    setResults(data);
+    setResults(result.repositories);
+    setLastPage(result.lastPage);
+    setSearchQuery(query);
     setLoading(false);
+  };
+
+  const fetchMore = async () => {
+    if (page >= lastPage) {
+      console.log("No more pages to fetch");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("q", searchQuery);
+    formData.append("page", (page + 1).toString());
+    const result = await SearchRepositoryAction(formData);
+    const repositories = result.repositories;
+    setResults((prevRepos) => [...prevRepos, ...repositories]);
+    setPage((prevPage) => {
+      const newPage = prevPage + 1;
+      return newPage;
+    });
   };
 
   return (
     <div>
       <SearchForm onSearch={handleSearch} />
-      {loading ? <Loading /> : <SearchResults results={results} />}
+      {loading ? <Loading /> : <SearchResultsList results={results} fetchMore={fetchMore} />}
     </div>
   );
 }
--- components/search/SearchResult.tsx
+++ components/search/SearchResult.tsx
@@ -1,13 +1,46 @@
 "use client";
 
-import SearchResult from "@/lib/repositories/responses/searchResult";
+import { useEffect, useRef, useState } from "react";
+import Loading from "@/app/loading";
+import Repository from "@/lib/repositories/responses/repository";
 
-export default function SearchResults({
+export default function SearchResultsList({
   results,
+  fetchMore,
 }: {
-  results: SearchResult[] | null;
+  results: Repository[];
+  fetchMore: () => Promise<void>;
 }) {
-  if (!results) {
+  const [isFetching, setIsFetching] = useState(false);
+  const bottomBoundaryRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const [entry] = entries;
+        if (entry.isIntersecting && !isFetching) {
+          setIsFetching(true); // データ取得中に設定
+          fetchMore().finally(() => setIsFetching(false));
+        }
+      },
+      // 完全にビューポート内に入ったときに発火
+      { threshold: 1.0 }
+    );
+
+    // bottomBoundaryRefが更新される可能性があるので一旦コピーする
+    const currentRef = bottomBoundaryRef.current;
+    if (currentRef) {
+      observer.observe(currentRef);
+    }
+
+    return () => {
+      if (currentRef) {
+        observer.unobserve(currentRef);
+      }
+    };
+  }, [fetchMore, isFetching]);
+
+  if (!results || results.length === 0) {
     return (
       <div className="flex items-center justify-center">
         <p className="text-gray-600">検索結果がありません。</p>
@@ -16,20 +49,24 @@ export default function SearchResults({
   }
 
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex flex-col items-center justify-center">
       <ul className="mt-6 w-full max-w-lg">
         {results.map((result) => (
           <li
             key={result.id}
             className="p-4 bg-white shadow-md rounded-lg mb-4"
           >
-            <h2 className="text-xl font-bold break-words">{result.repositoryFullName}</h2>
+            <h2 className="text-xl font-bold break-words">
+              {result.repositoryFullName}
+            </h2>
             <p className="text-gray-600">
               {result.description || "No description available"}
             </p>
           </li>
         ))}
       </ul>
+      <div ref={bottomBoundaryRef} className="h-16"></div>
+      {isFetching && <Loading />}
     </div>
   );
 }
