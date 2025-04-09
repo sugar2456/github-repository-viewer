38d4bb135e38ea1d0dc709bb52309e1461e772b9
sugar2456 <sugar2456_0526@yahoo.co.jp>
2025-04-09 21:25:49 +0900

    レスポンシブ対応

--- components/detail/RepositoryDetailComponent.tsx
+++ components/detail/RepositoryDetailComponent.tsx
@@ -3,24 +3,24 @@ import DetailInfo from "./DetailInfo";
 
 export default function RepositoryDetailComponent() {
   return (
-    <div className="grid grid-cols-4 grid-rows-2 gap-4 p-4 w-2/3">
-      <div className="col-span-4 row-span-1">
+    <div className="grid grid-cols-4 grid-rows-2 gap-4 p-4 lg:w-2/3 w-full">
+      <div className="col-span-4 row-span-2">
         <DetailInfo
           repositoryFullName="example/repo"
           langueage="JavaScript"
           iconUrl="https://avatars.githubusercontent.com/u/51810157?v=4"
         />
       </div>
-      <div className="col-span-1 row-span-1">
+      <div className="lg:col-span-1 md:col-span-2 col-span-4 row-span-1">
         <CountCard count={100} title="Star" />
       </div>
-      <div className="col-span-1 row-span-1">
+      <div className="lg:col-span-1 md:col-span-2 col-span-4 row-span-1">
         <CountCard count={50} title="Fork" />
       </div>
-      <div className="col-span-1 row-span-1">
+      <div className="lg:col-span-1 md:col-span-2 col-span-4 row-span-1">
         <CountCard count={200} title="Fork数" />
       </div>
-      <div className="col-span-1 row-span-1">
+      <div className="lg:col-span-1 md:col-span-2 col-span-4 row-span-1">
         <CountCard count={300} title="issue数" />
       </div>
     </div>
