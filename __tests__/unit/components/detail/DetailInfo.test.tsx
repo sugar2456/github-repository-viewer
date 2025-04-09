fab968a1ef8324311a58d21d104413197b27c434
sugar2456 <sugar2456_0526@yahoo.co.jp>
2025-04-09 20:40:10 +0900

    リポジトリ詳細情報を表示するコンポーネントを実装

---
+++ __tests__/unit/components/detail/DetailInfo.test.tsx
@@ -0,0 +1,40 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import DetailInfo from "@/components/detail/DetailInfo";
+
+jest.mock("next/image", () => ({
+  __esModule: true,
+  default: (props: React.ImgHTMLAttributes<HTMLImageElement>) => {
+    // eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text
+    return <img {...props} />;
+  },
+}));
+
+describe("DetailInfo", () => {
+  it("プロパティが正しくレンダリングされる", () => {
+    const mockRepositoryFullName = "owner/repo";
+    const mockLangueage = "TypeScript";
+    const mockIconUrl = "https://avatars.githubusercontent.com/u/123456?v=4";
+
+    render(
+      <DetailInfo
+        repositoryFullName={mockRepositoryFullName}
+        langueage={mockLangueage}
+        iconUrl={mockIconUrl}
+      />
+    );
+
+    // リポジトリ名が正しく表示されているか確認
+    const repositoryNameElement = screen.getByText(mockRepositoryFullName);
+    expect(repositoryNameElement).toBeInTheDocument();
+
+    // 言語が正しく表示されているか確認
+    const languageElement = screen.getByText(`言語: ${mockLangueage}`);
+    expect(languageElement).toBeInTheDocument();
+
+    // アイコン画像が正しく表示されているか確認
+    const imageElement = screen.getByAltText(`${mockRepositoryFullName} icon`);
+    expect(imageElement).toBeInTheDocument();
+    expect(imageElement).toHaveAttribute("src", mockIconUrl);
+  });
+});
---
+++ components/detail/DetailInfo.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+export default function DetailInfo({
+  repositoryFullName,
+  langueage,
+  iconUrl,
+}: {
+  repositoryFullName: string;
+  langueage: string;
+  iconUrl: string;
+}) {
+  return (
+    <div className="grid grid-cols-3 grid-rows-2 p-4 bg-white shadow-md rounded-lg w-1/2">
+      <Image
+        src={iconUrl}
+        alt={`${repositoryFullName} icon`}
+        width={64}
+        height={64}
+        className="row-span-2 mx-auto self-center"
+      />
+      <div className="col-span-2">
+        <h2 className="text-3xl font-bold pb-4">{repositoryFullName}</h2>
+      </div>
+      <div className="col-span-2">
+        <p className="text-lg font-semibold">言語: {langueage}</p>
+      </div>
+    </div>
+  );
+}
